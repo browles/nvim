403d77378d07504b1443754d5451349f3bb1d77e
browles <rowlesben@gmail.com>
2023-10-27 02:31:11 -0700

    Remove leader-T conflicts

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -12,9 +12,9 @@ local binds = {
   { "n", "<leader>ds", ":GoDbgStop<CR>" },
   { "n", "<leader>dk", ":GoDbgKeys<CR>" },
 
-  { "n", "<leader>tt", ":GoTestFunc<CR>" },
-  { "n", "<leader>tf", ":GoTestFile<CR>" },
-  { "n", "<leader>tp", ":GoTestPkg<CR>" },
+  { "n", "<leader>rr", ":GoTestFunc<CR>" },
+  { "n", "<leader>rf", ":GoTestFile<CR>" },
+  { "n", "<leader>rp", ":GoTestPkg<CR>" },
 }
 
 for _, bind in pairs(binds) do
