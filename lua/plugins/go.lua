eeb9a2794f5d104382c42628c9633fab82ea618d
browles <rowlesben@gmail.com>
2023-10-29 23:51:00 -0700

    Remove unused variable

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,6 +1,5 @@
 require("go").setup({})
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
