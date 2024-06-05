5701bbe36ce308a8989c4641e19f540c11cbcea3
browles <rowlesben@gmail.com>
2024-06-05 22:37:53 +0000

    Ignore gitcommit ft during nvim-tree update_root

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -2,6 +2,9 @@ require("nvim-tree").setup({
   sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
-    update_root = true,
+    update_root = {
+      enable = true,
+      ignore_list = { "gitcommit" },
+    },
   },
 })
